dd0aec03a1b2771314b92c2edbfdfa80ada4d3a1
Rhys Campbell <rhys.james.campbell@googlemail.com>
2017-11-07 16:58:25 +0100

    Modified main script. Needs further changes to get expect working fully

--- create_ozfs_appliance_env.sh
+++ create_ozfs_appliance_env.sh
@@ -1,37 +1,29 @@
 #!/bin/bash
-packer build packer-oracle-zfs-appliance1.json && echo "Finished building appliance #1";
-rmdir output-virtualbox-ovf && echo "Removed unneeded empty output file";
-packer build packer-oracle-zfs-appliance2.json && echo "Finished building appliance #2";
-rmdir output-virtualbox-ovf && echo "Removed unneeded empty output file";
-sleep 60 && echo "Sleeping for 60 seconds";
-
-# Runs a script against
-function run_ozfs_script() {
-    HOST=$1;
-    SCRIPT=$2;
-    expect <<- EOF
-      ssh root@$HOST < $SCRIPT
-      expect ': $'
-      send 'secret\r'
-EOF
-}
+#packer build packer-oracle-zfs-appliance1.json && echo "Finished building appliance #1";
+#rmdir output-virtualbox-ovf && echo "Removed unneeded empty output directory";
+#packer build packer-oracle-zfs-appliance2.json && echo "Finished building appliance #2";
+#rmdir output-virtualbox-ovf && echo "Removed unneeded empty output directory";
+#sleep 60 && echo "Sleeping for 60 seconds";
 
 # Start the vms up again
-VBoxManage startvm "packer-oracle-zfs1";
-VBoxManage startvm "packer-oracle-zfs2";
+#VBoxManage startvm "packer-oracle-zfs1";
+#VBoxManage startvm "packer-oracle-zfs2";
+
+# Sleep for a bit
+sleep 60
 
 # Configure the appliances
-run_ozfs_script 192.168.47.110 ozfs_config_scripts/1-create_pool.ksh;
-run_ozfs_script 192.168.47.111 ozfs_config_scripts/1-create_pool.ksh;
+ozfs_config_scripts/1-create_pool.exp 192.168.47.110;
+ozfs_config_scripts/1-create_pool.exp 192.168.47.111;
 
-run_ozfs_script 192.168.47.110 ozfs_config_scripts/2-create_project.ksh;
-run_ozfs_script 192.168.47.111 ozfs_config_scripts/2-create_project.ksh;
+ozfs_config_scripts/2-create_project.exp 192.168.47.110;
+ozfs_config_scripts/2-create_project.exp 192.168.47.111;
 
-run_ozfs_script 192.168.47.110 ozfs_config_scripts/3-create_filesystem.ksh;
-run_ozfs_script 192.168.47.111 ozfs_config_scripts/3-create_filesystem.ksh;
+ozfs_config_scripts/3-create_filesystem.exp 192.168.47.110;
+ozfs_config_scripts/3-create_filesystem.exp 192.168.47.111;
 
-run_ozfs_script 192.168.47.110 ozfs_config_scripts/4-create_replication_target.ksh
+ozfs_config_scripts/4-create_replication_target.exp 192.168.47.110;
 
-run_ozfs_script 192.168.47.110 ozfs_config_scripts/5-create_replication_action.ksh
+ozfs_config_scripts/5-create_replication_action.exp 192.168.47.110;
 
-run_ozfs_script 192.168.47.110 ozfs_config_scripts/6-create_test_data.ksh
+ozfs_config_scripts/6-create_test_data.exp 192.168.47.110;
